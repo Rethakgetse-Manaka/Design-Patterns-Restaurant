c760fd6243ae7480bd3e50a1a24d475d0dbf5d5b
MishkaD <132209049+MishkaD@users.noreply.github.com>
2023-11-05 16:40:52 +0200

    Add files via upload

--- Order.h
+++ Order.h
@@ -1,28 +1,43 @@
 #ifndef Order_H
 #define Order_H
-#include "FoodItem.h"
 // Include the OrderMemento class
 #include "OrderMemento.h" 
+#include <string>
 #include <vector>
+#include "FoodItem.h"
+// #include "SpecificCocktail.h"
+// #include "BaseCocktail.h"
 
-class Waiter;
-class StoredOrder;
+
 class Order
 {
     //added for memento
     private:
+    // int customerID;
+    // int tableID;
+    // std::string customerName;
+    // std::vector<SpecificCocktail*> drinks;
     std::vector<FoodItem*> items;
     bool isModified;
    
 
     public:
-        int tableNumber;
-        Waiter* waiter;
-        std::string custId;
-        Order();
+        //  Order(int cID, std::string cName);
+        // void addStarter(std::string, double val);
+        // void addItem(std::string item, bool g, bool s, double val, bool well);
+        // std::vector<FoodItem*> getItems();
+        // int getTableID();
+        // void setTableID(int id);
+        // std::vector<SpecificCocktail*> getDrinks();
+        // void addDrink(std::string baseName, bool alch, double cost, std::string mixes);
+        // int getCustomerID();
+        // double getTotal();
+        // std::string getCustomerName();
+        // std::string printOrder();
+        // ~Order();
+         Order();
         void addItem(FoodItem* i);
         void removeItem(FoodItem* i);
-        // Add a method to access items
         const std::vector<FoodItem*>& getItems() const;
        //added
         OrderMemento* createMemento();
