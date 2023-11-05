d9d6dc0c185dbb3accf0239eb83780c0dc6366bf
Loading31115 <132940697+Loading31115@users.noreply.github.com>
2023-11-05 10:53:51 +0200

    No input

--- RestuarantMain.cpp
+++ RestuarantMain.cpp
@@ -134,14 +134,14 @@ int main() {
     restuarant->initialCustomerHandling(customer2);
     restuarant->initialCustomerHandling(customer3);
 
-    customer1->placeOrder();
-    customer2->placeOrder();
-    customer3->placeOrder();
+    // customer1->placeOrder();
+    // customer2->placeOrder();
+    // customer3->placeOrder();
 
-    table1->readyForBill(waiter1,accountingSystem);
-    restuarant->payTab(customer1,careTaker);
-    restuarant->payTab(customer2,careTaker);
-    restuarant->payTab(customer3,careTaker);
-    accountingSystem->generateReport();
+    // table1->readyForBill(waiter1,accountingSystem);
+    // restuarant->payTab(customer1,careTaker);
+    // restuarant->payTab(customer2,careTaker);
+    // restuarant->payTab(customer3,careTaker);
+    // accountingSystem->generateReport();
     return 0;
 }
