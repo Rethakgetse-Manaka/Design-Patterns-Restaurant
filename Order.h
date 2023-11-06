3fc8935a73f0e62051970ef8cf3fe9ce183a2e97
MishkaD <132209049+MishkaD@users.noreply.github.com>
2023-11-06 10:49:51 +0200

    Add files via upload

--- Order.h
+++ Order.h
@@ -9,41 +9,68 @@
 // #include "BaseCocktail.h"
 
 
-class Order
-{
-    //added for memento
+
+/**
+ * @brief The Order class represents an order made by a customer in a restaurant.
+ * 
+ * The class contains a vector of FoodItem objects that represent the items ordered by the customer.
+ * It also has methods to add and remove items from the order, as well as to create and restore from a memento.
+ */
+class Order {
     private:
-    // int customerID;
-    // int tableID;
-    // std::string customerName;
-    // std::vector<SpecificCocktail*> drinks;
-    std::vector<FoodItem*> items;
-    bool isModified;
-   
+        std::vector<FoodItem*> items; /**< A vector of FoodItem objects representing the items ordered by the customer. */
+        bool isModified; /**< A boolean value indicating whether the order has been modified. */
 
     public:
-        //  Order(int cID, std::string cName);
-        // void addStarter(std::string, double val);
-        // void addItem(std::string item, bool g, bool s, double val, bool well);
-        // std::vector<FoodItem*> getItems();
-        // int getTableID();
-        // void setTableID(int id);
-        // std::vector<SpecificCocktail*> getDrinks();
-        // void addDrink(std::string baseName, bool alch, double cost, std::string mixes);
-        // int getCustomerID();
-        // double getTotal();
-        // std::string getCustomerName();
-        // std::string printOrder();
-        // ~Order();
-         Order();
+        /**
+         * @brief Default constructor for the Order class.
+         */
+        Order();
+
+        /**
+         * @brief Adds a FoodItem object to the order.
+         * 
+         * @param i A pointer to the FoodItem object to be added.
+         */
         void addItem(FoodItem* i);
+
+        /**
+         * @brief Removes a FoodItem object from the order.
+         * 
+         * @param i A pointer to the FoodItem object to be removed.
+         */
         void removeItem(FoodItem* i);
+
+        /**
+         * @brief Returns a constant reference to the vector of FoodItem objects representing the items ordered by the customer.
+         * 
+         * @return const std::vector<FoodItem*>& A constant reference to the vector of FoodItem objects representing the items ordered by the customer.
+         */
         const std::vector<FoodItem*>& getItems() const;
-       //added
+
+        /**
+         * @brief Creates a memento of the current state of the order.
+         * 
+         * @return OrderMemento* A pointer to the memento object created.
+         */
         OrderMemento* createMemento();
+
+        /**
+         * @brief Modifies the order.
+         */
         void modifyOrder();
+
+        /**
+         * @brief Cancels the order.
+         */
         void cancelOrder();
+
+        /**
+         * @brief Restores the state of the order from a memento.
+         * 
+         * @param memento A pointer to the memento object to restore from.
+         */
         void restoreFromMemento(OrderMemento* memento);
-       
 };
+
 #endif
