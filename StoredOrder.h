4d238a2e898eea2d65b4f703452ee7c8fe1b1943
MishkaD <132209049+MishkaD@users.noreply.github.com>
2023-11-06 10:46:08 +0200

    Add files via upload

--- StoredOrder.h
+++ StoredOrder.h
@@ -5,15 +5,29 @@
 
 #include <vector>
 
+/**
+ * @brief The StoredOrder class represents an object that stores a collection of OrderMemento objects.
+ */
 class StoredOrder
 {
 private:
     std::vector<OrderMemento*> mementos;
 
 public:
+    /**
+     * @brief Adds an OrderMemento object to the collection.
+     * 
+     * @param memento The OrderMemento object to be added.
+     */
     void storeMemento(OrderMemento* memento);
+
+    /**
+     * @brief Retrieves an OrderMemento object from the collection.
+     * 
+     * @param index The index of the OrderMemento object to be retrieved.
+     * @return The OrderMemento object at the specified index.
+     */
     OrderMemento* retrieveMemento(int index);
-   
 };
 
 #endif
