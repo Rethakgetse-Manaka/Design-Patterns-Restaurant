fa284f95cc2335e47a904ffdd1e71114d4c91637
MishkaD <132209049+MishkaD@users.noreply.github.com>
2023-11-06 10:49:38 +0200

    Add files via upload

--- OrderMemento.h
+++ OrderMemento.h
@@ -4,6 +4,10 @@
 #include "FoodItem.h"
 #include <vector>
 
+/**
+ * @brief The OrderMemento class represents a memento object that stores the state of an order.
+ * 
+ */
 class OrderMemento
 {
 private:
@@ -11,8 +15,26 @@ private:
     bool modified;
 
 public:
+    /**
+     * @brief Construct a new OrderMemento object.
+     * 
+     * @param items The vector of FoodItem pointers to be saved.
+     * @param isModified The boolean value indicating whether the order has been modified.
+     */
     OrderMemento(const std::vector<FoodItem*>& items, bool isModified); 
+
+    /**
+     * @brief Get the saved items vector.
+     * 
+     * @return std::vector<FoodItem*> The vector of saved FoodItem pointers.
+     */
     std::vector<FoodItem*> getSavedItems() const;
+
+    /**
+     * @brief Check if the order has been modified.
+     * 
+     * @return true if the order has been modified, false otherwise.
+     */
     bool isModified() const;
   
 };
