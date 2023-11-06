6fe6b94821b88c41de4570e7e1d115f0d60cbdc4
MishkaD <132209049+MishkaD@users.noreply.github.com>
2023-11-06 10:53:13 +0200

    Add files via upload

--- BaseCocktail.h
+++ BaseCocktail.h
@@ -2,15 +2,42 @@
 #define BASECOCKTAIL_H
 #include "Cocktail.h"
 
+/**
+ * @brief The BaseCocktail class is a concrete implementation of the Cocktail interface.
+ * 
+ */
 class BaseCocktail : public Cocktail {
 private:
-    std::string cocktailType;
-    double cocktailCost;
+    std::string cocktailType; /**< The type of the cocktail. */
+    double cocktailCost; /**< The cost of the cocktail. */
 
 public:
+    /**
+     * @brief Construct a new Base Cocktail object.
+     * 
+     * @param type The type of the cocktail.
+     * @param cost The cost of the cocktail.
+     */
     BaseCocktail(std::string type, double cost);
+
+    /**
+     * @brief Get the description of the cocktail.
+     * 
+     * @return std::string The description of the cocktail.
+     */
     std::string getDescription() const override;
+
+    /**
+     * @brief Get the cost of the cocktail.
+     * 
+     * @return double The cost of the cocktail.
+     */
     double cost() const override;
+
+    /**
+     * @brief Destroy the Base Cocktail object.
+     * 
+     */
     virtual ~BaseCocktail();
 };
 
