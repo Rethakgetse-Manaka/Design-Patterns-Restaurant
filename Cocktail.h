822243645f47d3ee1932cbd00fb4b317bd06ec01
MishkaD <132209049+MishkaD@users.noreply.github.com>
2023-11-06 10:55:16 +0200

    Add files via upload

--- Cocktail.h
+++ Cocktail.h
@@ -1,11 +1,31 @@
+/**
+ * @brief The Cocktail class is an abstract base class that defines the interface for all cocktails.
+ * 
+ */
 #ifndef COCKTAIL_H
 #define COCKTAIL_H
 #include <string>
 
 class Cocktail {
 public:
+    /**
+     * @brief Get the Description object
+     * 
+     * @return std::string 
+     */
     virtual std::string getDescription() const = 0;
+
+    /**
+     * @brief Get the cost object
+     * 
+     * @return double 
+     */
     virtual double cost() const = 0;
+
+    /**
+     * @brief Destroy the Cocktail object
+     * 
+     */
     virtual ~Cocktail();
 };
 
