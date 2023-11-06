d4385782669249a820abe6e69dc93b553fd8af0a
Loading31115 <132940697+Loading31115@users.noreply.github.com>
2023-11-06 15:37:39 +0200

    Update RestuarantMain.cpp

--- RestuarantMain.cpp
+++ RestuarantMain.cpp
@@ -126,23 +126,23 @@ int main() {
     restuarant->addWaiter(waiter2);
     restuarant->addWaiter(waiter3);
 
-    Customer* customer1 = new Customer(1,"Kamo");
-    Customer* customer2 = new Customer(2,"KB");
-    Customer* customer3 = new Customer(3,"Victor");
+    // Customer* customer1 = new Customer(1,"Kamo");
+    // Customer* customer2 = new Customer(2,"KB");
+    // Customer* customer3 = new Customer(3,"Victor");
 
-    restuarant->initialCustomerHandling(customer1);
-    restuarant->initialCustomerHandling(customer2);
-    // restuarant->initialCustomerHandling(customer3);
+    // restuarant->initialCustomerHandling(customer1);
+    // restuarant->initialCustomerHandling(customer2);
+    // // restuarant->initialCustomerHandling(customer3);
 
-    customer1->placeOrder();
-    customer2->placeOrder();
-    // customer3->placeOrder();
+    // customer1->placeOrder();
+    // customer2->placeOrder();
+    // // customer3->placeOrder();
 
-    table1->readyForBill(waiter1,accountingSystem);
-    restuarant->payTab(customer1,careTaker);
-    restuarant->payTab(customer2,careTaker);
-    // restuarant->payTab(customer3,careTaker);
-    accountingSystem->generateReport();
+    // table1->readyForBill(waiter1,accountingSystem);
+    // restuarant->payTab(customer1,careTaker);
+    // restuarant->payTab(customer2,careTaker);
+    // // restuarant->payTab(customer3,careTaker);
+    // accountingSystem->generateReport();
 
     return 0;
 }
