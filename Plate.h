d4e218ab777dc900f20d0c2ea8c06bda8c680cf2
Rethakgetse <Rethakgeste11@gmail.com>
2023-11-06 11:06:38 +0200

    Meal is removed causing errors

--- Plate.h
+++ Plate.h
@@ -1,7 +1,6 @@
 #ifndef Plate_h
 #define Plate_h
 
-#include "Meal.h"
 #include <vector>
 /**
  * @class Plate
@@ -9,7 +8,7 @@
  */
 class Plate {
     private:
-        std::vector<Meal*> m_Meals; /**< A vector to store food items. */
+        std::vector<std::string> m_Meals; /**< A vector to store food items. */
         int customerID;
         int plateID;
 
@@ -23,19 +22,19 @@ class Plate {
          * @brief Add a food item to the plate.
          * @param item The name of the food item to add.
          */
-        void addMeal(Meal* item);
+        void addMeal(std::string* item);
 
         /**
          * @brief Remove a specific food item from the plate.
          * @param item The name of the food item to remove.
          */
-        void removeMeal(Meal* item);
+        void removeMeal(std::string* item);
 
         /**
          * @brief Get a pointer to the array of food items on the plate.
          * @return A pointer to the array of food items.
          */
-        std::vector<Meal*>& getMeals();
+        std::vector<std::string*>& getMeals();
 
         /**
          * @brief Destructor for the Plate class.
